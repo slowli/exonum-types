02f3db209b94f550061cabdf95f84cd27c1e2217
Alex Ostrovski <ostrovski.alex@gmail.com>
2017-09-20 18:28:00 +0300

    Add comments and refactor common utils

--- src/lowlevel/common.js
+++ src/lowlevel/common.js
@@ -42,6 +42,11 @@ const EXONUM_TYPEREF_PROP = typeof Symbol !== 'undefined'
   ? Symbol.for('exonum.typeref')
   : '__exonumTyperef'
 
+/**
+ * Retrieves the Exonum type corresponding to this Exonum object.
+ *
+ * @param {ExonumType} exonumObj
+ */
 export function getType (exonumObj) {
   if (!isExonumObject(exonumObj)) return undefined
   return exonumObj[EXONUM_TYPEREF_PROP]
@@ -51,10 +56,31 @@ const EXONUM_RAW_PROP = typeof Symbol !== 'undefined'
   ? Symbol.for('exonum.raw')
   : '__exonumRaw'
 
+/**
+ * Sets the "raw" value of an Exonum type instance. The raw value may be used
+ * internally within the type definition, and is also exposed to external users
+ * (e.g., by accessing fields of `struct`s).
+ *
+ * @param {Object} obj
+ * @param {any} raw
+ *   A raw value for internal use. Will be returned by reference.
+ * @param {?() => any} clone
+ *   A function to export the raw value for external use. If the raw value is not
+ *   immutable, this function should clone the "internal" raw value (hence the name).
+ */
 export function setRawValue (obj, raw, clone = () => raw) {
   Object.defineProperty(obj, EXONUM_RAW_PROP, { value: { data: raw, clone } })
 }
 
+/**
+ * Retrieves the "raw" value of an Exonum type instance previously set by `setRawValue()`.
+ *
+ * @param {Object} obj
+ * @param {boolean} externalUse
+ *   The context to use the retrieved value
+ * @returns {any}
+ *   The stored raw value, or `undefined` if the object does not have one
+ */
 export function rawValue (obj, externalUse = false) {
   if (!obj[EXONUM_RAW_PROP]) {
     return undefined
@@ -62,6 +88,16 @@ export function rawValue (obj, externalUse = false) {
   return externalUse ? obj[EXONUM_RAW_PROP].clone() : obj[EXONUM_RAW_PROP].data
 }
 
+/**
+ * Retrieves the "raw" value of an Exonum type instance previously set by `setRawValue()`,
+ * or returns the original instance if it does not have the raw value.
+ *
+ * @param {Object} obj
+ * @param {boolean} externalUse
+ *   The context to use the retrieved value
+ * @returns {any}
+ *   The stored raw value, or the original object
+ */
 export function rawOrSelf (obj, externalUse = false) {
   return (obj && obj[EXONUM_RAW_PROP] !== undefined) ? rawValue(obj, externalUse) : obj
 }
@@ -83,10 +119,40 @@ export function memoize (fn) {
   }
 }
 
-export function initType (Type, { name, byteLength, proxiedMethods }) {
-  class T extends Type {
+/**
+ * Initializes an Exonum type by attaching some sensible default method implementations.
+ *
+ * @param {Class<ExonumBase>} Type
+ *   The base class to build the Exonum type upon. Should implement the following methods:
+ *
+ *   - `serialize(buffer: Uint8Array): void`
+ *   - `toJSON(): any`
+ *
+ *   If the type has variable serialization length, `byteLength(): number` method should
+ *   be defined as well.
+ * @param {Object} options
+ * @param {?string} options.name
+ *   Type name; used to get string representation of the class
+ * @param {number | void} options.typeLength
+ *   The length of the type serialization for fixed-length types. For var-length types,
+ *   should be `undefined`.
+ * @param {?Array<string>} options.proxiedMethods
+ *   Methods to proxy from the `rawValue()` of the type instances.
+ */
+export function initType (Type, { name, typeLength, proxiedMethods }) {
+  if (!name) name = '[Exonum type]'
+
+  class ExonumType extends Type {
+    static inspect () {
+      return name
+    }
+
+    static toString () {
+      return name
+    }
+
     static typeLength () {
-      return byteLength
+      return typeLength
     }
 
     static from (maybeInstance) {
@@ -97,7 +163,7 @@ export function initType (Type, { name, byteLength, proxiedMethods }) {
     }
 
     byteLength () {
-      return (byteLength !== undefined) ? byteLength : super.byteLength()
+      return (typeLength !== undefined) ? typeLength : super.byteLength()
     }
 
     serialize (buffer) {
@@ -107,28 +173,24 @@ export function initType (Type, { name, byteLength, proxiedMethods }) {
     }
   }
 
-  T.prototype.byteLength = memoize(T.prototype.byteLength)
+  ExonumType.prototype.byteLength = memoize(ExonumType.prototype.byteLength)
 
-  Object.defineProperty(T, EXONUM_KIND_PROP, { value: 'type' })
-  Object.defineProperty(T.prototype, EXONUM_KIND_PROP, { value: 'object' })
-  Object.defineProperty(T.prototype, EXONUM_TYPEREF_PROP, { value: T })
+  Object.defineProperty(ExonumType, EXONUM_KIND_PROP, { value: 'type' })
+  Object.defineProperty(ExonumType.prototype, EXONUM_KIND_PROP, { value: 'object' })
+  Object.defineProperty(ExonumType.prototype, EXONUM_TYPEREF_PROP, { value: ExonumType })
 
-  if (!name) {
-    name = '[Exonum type]'
-  }
-  T.inspect = () => name
-  T.toString = () => name
-
+  // XXX: Is it possible to get rid of `proxiedMethods` and perform movement of methods
+  // individually in each case?
   if (proxiedMethods) {
     proxiedMethods.forEach(name => {
-      T.prototype[name] = function () {
+      ExonumType.prototype[name] = function () {
         const raw = rawValue(this)
         return raw[name].apply(raw, arguments)
       }
     })
   }
 
-  return T
+  return ExonumType
 }
 
 /**
