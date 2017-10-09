559858c3eadc95e28878bda1b96bf7de711fc3e5
Alex Ostrovski <ostrovski.alex@gmail.com>
2017-10-09 21:51:25 +0300

    Refactor `MapView` and add comments

--- src/MapView.js
+++ src/MapView.js
@@ -4,15 +4,46 @@ import { createType, rawValue } from './lowlevel/common'
 import initFactory from './lowlevel/initFactory'
 import { hash } from './crypto'
 
+/**
+ * Calculates the hash for a tree with a single key.
+ *
+ * @param {Bits256} key
+ * @param {Uint8Array} nodeHash
+ * @returns {Uint8Array}
+ */
 function stubHash (key, nodeHash) {
   return hash(key, nodeHash)
 }
 
+/**
+ * Calculates the hash for a tree node.
+ *
+ * @param {TreeNode} node
+ * @returns {Uint8Array}
+ */
 function nodeHash (node) {
   return hash(node.leftHash, node.rightHash, node.leftKey, node.rightKey)
 }
 
+/**
+ * Node in a Merkle Patricia tree backing the map view.
+ */
 class TreeNode {
+  /**
+   * Creates a root node with a specified child key for a leftmost child in the map view.
+   *
+   * @param {Bits256} emptyKey
+   * @param {Bits256} key
+   */
+  static root (emptyKey, key) {
+    return new this({
+      key: emptyKey,
+      leftKey: null,
+      leftHash: null,
+      rightKey: key
+    })
+  }
+
   constructor ({ key, leftKey, leftHash, rightKey }) {
     this.key = key
     this.leftKey = leftKey
@@ -20,29 +51,63 @@ class TreeNode {
     this.leftHash = leftHash
   }
 
+  /**
+   * Truncates the right key of the node to the specified length.
+   *
+   * @param {number} toBits
+   */
   truncateRightKey (toBits) {
     this.rightKey = this.rightKey.truncate(toBits)
   }
 
+  /**
+   * Finalizes the node. Finalization takes the hash of the right child of the node
+   * and returns the hash of the node itself.
+   *
+   * @param {Uint8Array} rightHash
+   * @returns {Uint8Array}
+   */
   finalize (rightHash) {
-    this.rightHash = rightHash
-    return nodeHash(this)
+    if (!this.leftHash) {
+      // Special case for the root node: it can be "finalized" 2 times (for the left and right subtrees).
+      // This is the first finalization.
+
+      this.leftKey = this.rightKey
+      this.leftHash = rightHash
+      this.rightKey = null
+      return rightHash
+    } else {
+      this.rightHash = rightHash
+      return nodeHash(this)
+    }
   }
 }
 
 /**
+ * Computes the root hash of the Merkle Patricia tree backing the specified entries
+ * in the map view.
+ *
+ * The tree is not restored in full; instead, we add the keys to
+ * the tree in their lexicographic order (i.e., according to the `Bits256.comparator()` function)
+ * and keep track of the rightmost nodes (the right contour) of the tree.
+ * It is easy to see that adding keys in the lexicographic order means that only
+ * the nodes in the right contour may be updated on each step. Further, on each step
+ * zero or more nodes are evicted from the contour, and a single new node is
+ * added to it.
+ *
  * @param {List<[Bits256, ?Hash]>} entries
- *  Guaranteed to be sorted by increasing `Bits256` component
+ *   Guaranteed to be sorted by increasing `Bits256` component
  */
 function treeHash (entries, emptyKey) {
   if (entries.count() === 0) {
+    // Empty tree
     return new Uint8Array(32)
   } else if (entries.count() === 1) {
+    // Tree with a single node
     const { 0: key, 1: hash } = entries.get(0)
     if (!key.isTerminal) {
       throw new Error('Invalid map view: non-terminal isolated node')
     }
-
     return stubHash(key, hash)
   }
 
@@ -57,33 +122,9 @@ function treeHash (entries, emptyKey) {
     }
   }
 
-  // Add the first entry into the right contour
-  let lastKey = entries.get(0)[0]
-  let lastHash = entries.first()[1]
-
-  const root = {
-    key: emptyKey,
-    leftKey: null,
-    rightKey: lastKey,
-    leftHash: null,
-
-    truncateRightKey (bitLength) {
-      this.rightKey = this.rightKey.truncate(bitLength)
-    },
-
-    finalize (hash) {
-      // The root node is special: it can be "finalized" 2 times (for the left and right subtree).
-      if (!this.leftHash) {
-        this.leftKey = this.rightKey
-        this.leftHash = hash
-        this.rightKey = null
-        return hash
-      } else {
-        this.rightHash = hash
-        return nodeHash(this)
-      }
-    }
-  }
+  // Add the first entry into the right contour. This entry is the root of the tree.
+  let { 0: prevKey, 1: prevHash } = entries.first() // track the most recently added node
+  const root = TreeNode.root(emptyKey, prevKey)
 
   let rightContour = [ root ]
   rightContour.last = function () {
@@ -92,23 +133,24 @@ function treeHash (entries, emptyKey) {
 
   for (let i = 1; i < entries.count(); i++) {
     const { 0: key, 1: hash } = entries.get(i)
-    if (!hash) {
-      continue
-    }
+    if (!hash) continue
 
-    const commonPrefix = lastKey.commonPrefix(key)
+    const commonPrefix = prevKey.commonPrefix(key)
 
     // `finHash` and `finKey` are the characteristics of the last finalized node
     // in the contour
-    let [finHash, finKey] = [lastHash, lastKey]
+    let [finHash, finKey] = [prevHash, prevKey]
     while (rightContour.length > 0 && rightContour.last().key.bitLength() >= commonPrefix.bitLength()) {
-      const last = rightContour.pop()
-      finHash = last.finalize(finHash)
-      finKey = last.key
+      const node = rightContour.pop();
+      // It is easy to see that `finHash` is the final value of the `node.rightHash`;
+      // it cannot be updated during the further insertions to the tree.
+      // Thus, we may calculate the hash of the node and evict it from the contour.
+      [finHash, finKey] = [node.finalize(finHash), node.key]
     }
 
     if (rightContour.length > 0) {
-      rightContour[rightContour.length - 1].truncateRightKey(commonPrefix.bitLength())
+      rightContour.last().truncateRightKey(commonPrefix.bitLength())
+
       const newBranch = new TreeNode({
         key: commonPrefix,
         leftKey: finKey,
@@ -123,11 +165,12 @@ function treeHash (entries, emptyKey) {
       rightContour.push(root)
     }
 
-    lastKey = key
-    lastHash = hash
+    [prevKey, prevHash] = [key, hash]
   }
 
-  let finHash = lastHash
+  // Iteratively finalize all remaining nodes in the tree. This handles the special case
+  // when all keys start with the same bit(s); see the special clause in `TreeNode.finalize()`.
+  let finHash = prevHash
   while (rightContour.length > 0) {
     finHash = rightContour.pop().finalize(finHash)
   }
@@ -176,7 +219,7 @@ function mapView ({ K: KeyType, V: ValType, hashKeys = true }, resolver) {
           .filter(e => e.type === 'entry')
           .map(e => [e.entry.getOriginal('key'), e.entry.value])),
         missing: ImmutableSet(entryList.filter(e => e[1] === null).map(e => e[0]))
-      })
+      }, null)
 
       rawValue(this).hash = treeHash(entries, Bits256.from(''))
     }
